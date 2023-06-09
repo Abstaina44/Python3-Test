9cc75bbbc2902d63500e4727666cbb4a1c55a150
Eph <48015890+Abstaina44@users.noreply.github.com>
2023-06-09 08:37:22 +0000

    Python Script File.

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,4 @@
+#!/usr/bin/pyhton3
+import sys
+sys.stderr.write("and that piece of art is usedful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# commets
+
+import this
